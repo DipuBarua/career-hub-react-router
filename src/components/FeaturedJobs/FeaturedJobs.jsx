0f6038b9e1e36e3dcd474ba169e61a2e85d0a8b8
DipuBarua <you@example.com>
2023-09-24 15:02:52 +0600

    added show all btn

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -9,13 +9,22 @@ const FeaturedJobs = () => {
             .then(res => res.json())
             .then(data => setJobs(data));
     }, []);
+    // not best way to show all 
+    const [dataLength, setDataLength] = useState(4);
 
     return (
         <div>
             <h2 className=" text-5xl font-extrabold">Featured Jobs: {jobs.length}</h2>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className=" grid grid-cols-2 gap-6">
-                {jobs.map(job => <Job key={job.id} job={job}></Job>)}
+                {jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)}
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <button
+                    className=" btn-primary p-4 rounded-lg"
+                    onClick={() => setDataLength(jobs.length)}>
+                    Show All
+                </button>
             </div>
         </div>
     );
